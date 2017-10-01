fa7a26690831fae43031cb87282acba4aa7aba09
mndvn <omer.mindivanli@gmail.com>
2017-10-02 01:54:35 +0300

    Post UI Fixed

--- main.js
+++ main.js
@@ -1,4 +1,22 @@
 // Create Post(); Func;
-function Post(){
-    alert("PostWorks");
-}
+    function isPost() {
+        // 3 Steps of JavaScript Dom Create var Element, append and property
+        // 1.Creating
+        var postFrame = document.createElement("div");
+        var textBtnIs = document.createTextNode("The First meaningful Java Script!");
+        postFrame.setAttribute("id", "postFrame_id");
+        // 2.Appending
+        document.body.appendChild(postFrame);
+        postFrame.appendChild(textBtnIs);//this.TextNode will be the name of btn object
+        // 3.Adding Property
+        postFrame.style.backgroundColor = "gray";
+        postFrame.style.fontSize = "25px";
+        postFrame.style.fontWeight = "600";
+        postFrame.style.margin = "30px";
+        postFrame.style.padding = "30px";
+        postFrame.style.color = "white";
+    }
+
+    function isPostRemover(){
+        document.getElementById("postFrame_id").remove();
+    }
